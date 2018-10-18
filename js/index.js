39b2020757395e307624d2aa13b9ce4fd3f62786
Neel Shah <neelshah400@gmail.com>
2018-10-18 16:50:40 -0400

    fix iframe url error

--- js/index.js
+++ js/index.js
@@ -1,19 +1,21 @@
-var url = window.location.href;
-if(url.indexOf("index.html") === -1){
-    window.location.href += "index.html";
-    url = window.location.href;
-}
-else if(url.indexOf("?page=") === -1){
-    window.location.href += "?page=";
-    url = window.location.href;
-}
-var file = url.substring(url.lastIndexOf('=')+1);
-if(file === ""){
-    window.location.href += "home";
-    url = window.location.href;
-}
-
 function contentLoader(){
+    var url = window.location.href;
+    if(url.indexOf("index.html") === -1){
+        window.location.href += "index.html";
+        url = window.location.href;
+    }
+    else if(url.indexOf("?page=") === -1){
+        window.location.href += "?page=";
+        url = window.location.href;
+    }
+    var file = url.substring(url.lastIndexOf('=')+1);
+    if(file === ""){
+        window.location.href += "home";
+        url = window.location.href;
+    }
+//}
+
+//function contentLoader(){
     $("#content").load("./html/" + file + ".html");
     document.getElementById(file).classList.add("active");
 };
