b6dcf86a231b274d34906963bfb03cc081a1d939
Neel Shah <34845513+neelshah400@users.noreply.github.com>
2021-08-24 23:02:47 -0400

    Fix linting issue

--- src/components/ListCard.tsx
+++ src/components/ListCard.tsx
@@ -3,6 +3,7 @@ import { Icon as Iconify } from '@iconify/react'
 import React from 'react'
 
 const ListCard = ({ name, icon, skills }) => {
+  const color = useColorModeValue("gray.700", "gray.200")
   return (
     <Box w="sm" mx="auto" bg={useColorModeValue("white", "gray.800")} shadow="lg" rounded="lg" overflow="hidden">
       <Flex alignItems="center" px={6} py={3} bg="gray.900">
@@ -11,7 +12,7 @@ const ListCard = ({ name, icon, skills }) => {
       </Flex>
       <Box pb={4} px={6}>
         {skills.map(skill => (
-          <Flex key={skill.name} alignItems="center" mt={4} color={useColorModeValue("gray.700", "gray.200")}>
+          <Flex key={skill.name} alignItems="center" mt={4} color={color}>
             <Iconify icon={skill.icon} height={24} width={24} />
             <Text px={2} fontSize="lg">{skill.name}</Text>
           </Flex>
